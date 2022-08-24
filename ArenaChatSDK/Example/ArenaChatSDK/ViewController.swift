8cecfba29ae346d2742b664e3420d4cba90a31a6
Erick Vicente <vicente.erick@gmail.com>
2022-08-24 15:14:38 -0300

    Create Cell and Example

--- ArenaChatSDK/Example/ArenaChatSDK/ViewController.swift
+++ ArenaChatSDK/Example/ArenaChatSDK/ViewController.swift
@@ -1,16 +1,46 @@
 import UIKit
+import ArenaChatSDK
 
 class ViewController: UIViewController {
-
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(ChatCell.self, forCellReuseIdentifier: "ChatCell")
+        tableView.showsVerticalScrollIndicator = false
+        //tableView.estimatedRowHeight = UITableView.automaticDimension
+        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.estimatedRowHeight = UITableViewAutomaticDimension
+        tableView.separatorStyle = .none
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+        
+        tableView.reloadData()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ChatCell", for: indexPath) as! ChatCell
+        return cell
+    }
+}
+
+extension ViewController: UITableViewDelegate { }
 
